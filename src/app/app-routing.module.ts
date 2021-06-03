9793526695ec29e0dc0686b7f4e43738f8fd585d
trvani <safak.kaya@outlook.com>
2021-06-03 14:35:42 +0300

    Giriş Komponentleri ve navigasyon
    1. Navigasyon Eklendi,
    2.Users komponenti eklendi,
    3. Edit User komponenti eklendi,
    4. Login komponenti eklendi,
    5. Register komponenti eklendi,
    6. settings komponenti eklendi,
    7. sidebar komponenti eklendi,
    
    Navigasyon aktif hale getirildi. App-Routing modülü eklendi. Fonksiyonel hale getirildi.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+
+const routes: Routes = [
+  {path: '',component:DashboardComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'register',component:RegisterComponent},
+  {path: 'users/add',component:AddUserComponent},
+  {path: 'users/edit/:id',component:EditUserComponent},
+  {path: 'users/:id',component:UserDetailComponent},
+  {path: 'settings',component:SettingsComponent},
+  {path: '**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    UsersComponent,
+    SidebarComponent,
+    AddUserComponent,
+    EditUserComponent,
+    UserDetailComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
