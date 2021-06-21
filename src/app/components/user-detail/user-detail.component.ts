7bec0683b8925ef930658815b4c639ee2d4f8e0e
trvani <safak.kaya@outlook.com>
2021-06-21 22:44:13 +0300

    edit user
    Edit user page is creating

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import {Router,ActivatedRoute, Params} from '@angular/router';
+import {FlashMessagesModule, FlashMessagesService} from 'angular2-flash-messages';
+import { UserService } from '../../services/user.service';
+
+import { User } from '../../models/users';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  id!: string;
+  user!: User;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {
+
+   }
 
   ngOnInit(): void {
+    //get id from url
+    this.id = this.route.snapshot.params['id'];
+
+    //get client
+    this.userService.getUser(this.id).subscribe(user => {
+      this.user = user;
+    })
   }
 
 }
